0a13b21431a3cd424302c5c68efcddc6d8ae9987
vaghelaohm <vaghelaohm@gmail.com>
2024-08-31 14:49:36 +0530

    permission check

--- fileTest/urls.py
+++ fileTest/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_files
+from .views import list_files,check_file_permissions
 
 urlpatterns = [
     # ... your other url patterns
     path('list-files/', list_files, name='list_files'),
+    path('check-permissions/', check_file_permissions, name='check_permissions'),
 ]
--- fileTest/views.py
+++ fileTest/views.py
@@ -6,3 +6,12 @@ def list_files(request):
     media_path = os.path.join(settings.MEDIA_ROOT, 'profile_picture')
     files = os.listdir(media_path)
     return JsonResponse({'files': files})
+
+
+def check_file_permissions(request):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'profile_picture', 'base.jpg')
+    if os.path.exists(file_path):
+        permissions = oct(os.stat(file_path).st_mode)[-3:]
+        return JsonResponse({'file_exists': True, 'permissions': permissions})
+    else:
+        return JsonResponse({'file_exists': False, 'permissions': None})
